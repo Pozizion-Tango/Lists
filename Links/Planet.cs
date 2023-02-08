da12a310d91e86d9311254be7b2f2a82cc5c3bc7
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2023-02-08 13:55:48 +0100

    Add project files.

---
+++ Links/Planet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    //Merkur, Jorden, Mars, Jupiter, Saturn, Uranus, Neptun, Pluto
+    internal class Planet
+    {
+        public List<string> planet = new List<string> {  "Merkur","Jorden","Mars","Jupiter","Saturn","Uranus","Neptun","Pluto" };
+        public List<string> Addlist()
+        {
+            planet.Insert(1, "Venus");
+            return planet;
+        }
+    }
+}
---
+++ Links/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet solarsystem = new Planet();
+            int count = 0;
+            int plutonumber = 0;
+            List<string> planetcollection = solarsystem.Addlist();
+            foreach (string planet in planetcollection)
+            {
+                if (planet == "Pluto")
+                    plutonumber = count;
+                Console.WriteLine(planet);
+                count++;
+            }
+            Console.WriteLine(planetcollection.Count);
+            planetcollection.Remove("Pluto");
+            Console.ReadLine();
+            //------------------------------------------------
+
+
+            foreach (string planet in planetcollection)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.WriteLine(planetcollection.Count);
+            Console.ReadLine();
+            //------------------------------------------------
+
+
+            planetcollection.Insert(plutonumber, "Pluto");
+            foreach (string planet in planetcollection)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.WriteLine(planetcollection.Count);
+            Console.ReadLine();
+            //------------------------------------------------
+
+
+            List<string> planettemp = solarsystem.Addlist();
+            planettemp.Remove("Merkur");
+            planettemp.Remove("Jorden");
+            planettemp.Remove("Venus");
+            foreach (string planet in planettemp)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.WriteLine(planettemp.Count);
+            Console.ReadLine();
+            //------------------------------------------------
+
+
+            List<string> planetdiameter = new List<string>();
+            planetdiameter.Add("Venus");
+            planetdiameter.Add("Jorden");
+            planetdiameter.Add("Mars");
+            planetdiameter.Add("Neptun");
+            foreach (string planet in planetdiameter)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.WriteLine(planetdiameter.Count);
+            Console.ReadLine();
+            //------------------------------------------------
+
+
+            planetcollection.Clear();
+            planettemp.Clear();
+            planetdiameter.Clear();
+            Console.WriteLine(planetcollection.Count);
+            Console.WriteLine(planettemp.Count);
+            Console.WriteLine(planetdiameter.Count);
+            Console.ReadLine();
+        }
+    }   //Yea this was a fucking weird "test".
+}
+
